b841b0ba8992e3af1bac577b0c46786347f1d9d0
pat killean <pat.killean@gmail.com>
2025-08-22 11:21:01 -0400

    ref flightmanual content, add emitter & disposable ctors

--- src/pulsar_kit/exports/event_kit.cljs
+++ src/pulsar_kit/exports/event_kit.cljs
@@ -2,5 +2,14 @@
 
 (def _atom (js/require "atom")) ;; event-kit is exported, we cannot require it
 
+; https://flight-manual.atom-editor.cc/api/v1.63.1/Disposable/
+(defn disposable []
+  (new (.-Disposable _atom)))
+
+; https://flight-manual.atom-editor.cc/api/v1.63.1/CompositeDisposable/
 (defn composite-disposable []
   (new (.-CompositeDisposable _atom)))
+
+;; https://flight-manual.atom-editor.cc/api/v1.63.1/Emitter/
+(defn emitter []
+  (new (.-Emitter _atom)))
