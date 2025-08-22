07dccb60897df38a053a28f50c7762e61635fbbb
pat killean <pat.killean@gmail.com>
2025-08-22 11:25:25 -0400

    add clear-console command example to template

--- resources/template/src/ident/main.cljs
+++ resources/template/src/ident/main.cljs
@@ -1,7 +1,14 @@
 (ns {{IDENT}}.main
   (:require [pulsar-kit.electron.processes.main.browser-window.web-contents :as web-contents]
+            [pulsar-kit.environment.commands :as commands]
             [pulsar-kit.environment.notifications :as notifications]
             ["path" :as path]))
+
+(def command-tuples
+  [
+   ["atom-workspace" "{{IDENT}}:clear-console" (.bind js/console.clear js/console)]
+  ])
+
 
 (defonce worker (js/Worker. (.resolve path js/window.__dirname "{{PACKAGE_PATH}}/target/{{PATH_IDENT}}/worker/worker.js")))
 
@@ -14,6 +21,7 @@
   (try
     (do
       (js/console.log "activating {{IDENT}} package...")
+      (commands/register! command-tuples)
       (web-contents/open-devtools {:activate false})
       (notifications/add-success "{{IDENT}} package ready!" {:dismissable true}))
     (catch js/Error e
